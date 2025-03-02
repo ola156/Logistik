adc8c99c79b2a13588ada73d7957051fc9d2b7a1
Agboola Usman <agboolausman58@gmail.com>
2025-03-02 16:12:45 +0100

    project setup

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import logoShape from "../assets/logo-shape.svg";
+const Nav = () => {
+  return (
+    <header>
+      <div className="">
+        <img src={logo} alt="" className="bg-orange-200" />
+      </div>
+    </header>
+  );
+};
+
+export default Nav;

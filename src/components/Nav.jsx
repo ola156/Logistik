effd83bccc098cf6e3032e791dbb4f56a8503f8c
Agboola Usman <agboolausman58@gmail.com>
2025-03-05 23:34:46 +0100

    made navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.svg";
 import { FiPhone } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaXTwitter } from "react-icons/fa6";
 import { RiInstagramFill } from "react-icons/ri";
-
+import { MdSegment } from "react-icons/md";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { NavLink } from "react-router";
 
 const Nav = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <>
-      <div className="p-3 bg-black flex items-center justify-evenly">
-        <div className="text-white flex items-center ml-4">
+      <div className="py-3 bg-black md:flex items-center justify-evenly hidden">
+        <div className="text-white flex items-center ml-4 ">
           <FiPhone className="mx-2 text-orange-500" size={20} />
           <h3 className="font-extrabold mr-1">Phone :</h3>
-          <p className="font-semibold">+234 808-744-2174</p>
+          <p className="font-semibold hover:text-orange-500">
+            +234 808-744-2174
+          </p>
         </div>
         <div className="text-white flex items-center ml-4">
           <MdOutlineEmail className="mx-2 text-orange-500" size={20} />
           <h3 className="font-extrabold mr-1">Email :</h3>
-          <p className="font-semibold">agboolausman01@gmail.com</p>
+          <p className="font-semibold  hover:text-orange-500">
+            agboolausman01@gmail.com
+          </p>
         </div>
         <div className="text-white flex items-center ml-4">
-          <FaXTwitter className="mx-2" size={20} />
-          <RiInstagramFill className="mx-2 " size={20} />
-          <FaFacebookF className="mx-2 " size={20} />
-          <FaLinkedinIn className="mx-2 " size={20} />
+          <FaXTwitter className="mx-2  hover:text-orange-500" size={20} />
+          <RiInstagramFill className="mx-2  hover:text-orange-500" size={20} />
+          <FaFacebookF className="mx-2  hover:text-orange-500 " size={20} />
+          <FaLinkedinIn className="mx-2  hover:text-orange-500" size={20} />
         </div>
       </div>
-      <header className="w-full bg-orange-600 px-2 py-4 shadow">
+      <header className="w-full bg-orange-500 px-2 py-4 shadow">
         <nav className="max-w-[90%] mx-auto ">
           <div className="flex justify-between items-center">
             <div className="">
-              <img src={logo} alt="" className="w-40" />
+              <img src={logo} alt="" className="sm:w-40 w-30" />
             </div>
-            <ul className="flex justify-between gap-10">
-              <li>Home</li>
-              <li>Home</li>
-              <li>Home</li>
-              <li>Home</li>
+            <div className="md:hidden block">
+              <MdSegment
+                className="text-white hover:text-black"
+                size={25}
+                onClick={() => setIsNavOpen(true)}
+              />
+            </div>
+            <ul className="md:flex justify-between gap-10 lg:mr-25 hidden transition-colors text-white font-bold uppercase">
+              <li className="hover:text-black underline-offset-3 hover:underline">
+                Home
+              </li>
+              <li className="hover:text-black underline-offset-3 hover:underline">
+                Service
+              </li>
+              <li className="hover:text-black underline-offset-3 hover:underline">
+                About
+              </li>
+              <li className="hover:text-black underline-offset-3 hover:underline">
+                Contact
+              </li>
             </ul>
           </div>
         </nav>
+        {isNavOpen && (
+          <div className="fixed border-2 border-left border-l-orange-500 border-r-0 border-t-0 border-b-0 h-full w-[55%]  top-0 right-0 md:hidden block">
+            <div>
+              <div className=" bg-black mb-5 px-3 py-5  flex justify-between items-center">
+                <img src={logo} alt="" className="w-30" />
+                <IoMdCloseCircleOutline
+                  className="text-white hover:text-orange-500"
+                  size={22}
+                  onClick={() => setIsNavOpen(false)}
+                />
+              </div>
+
+              <ul className="flex flex-col justify-between gap-4  px-6 py-3 transition-colors text-black font-bold uppercase">
+                <NavLink to="/">
+                  <li className="text-black ">Home</li>
+                </NavLink>
+                <NavLink to="/">
+                  <li className="text-black">Service</li>
+                </NavLink>{" "}
+                <NavLink to="/">
+                  <li className="text-black">About</li>
+                </NavLink>{" "}
+                <NavLink to="/">
+                  <li className="text-black">Contact</li>
+                </NavLink>
+              </ul>
+            </div>
+          </div>
+        )}
       </header>
     </>
   );
